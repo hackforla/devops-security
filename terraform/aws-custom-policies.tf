adca44aa384ed9526ccc6bf152944d6ce062214d
Max Gonzalez <gmgonzal@users.noreply.github.com>
2025-08-13 17:12:56 -0700

    create mfa enforcement policy and add it to existing groups

--- terraform/aws-custom-policies.tf
+++ terraform/aws-custom-policies.tf
@@ -4,6 +4,10 @@ module "aws_custom_policies" {
     "IAMServicesSupervisor" = {
       description = "Policy granting IAM services admins permissions to make changes to user accounts"
       filename    = "level-4-iam-services-supervisor-policy.json"
+    },
+    "EnforceMFAForUsers" = {
+      description = "Policy enforcing MFA for devops security users"
+      filename    = "enforce-mfa-for-users-policy.json"
     }
   }
 }
