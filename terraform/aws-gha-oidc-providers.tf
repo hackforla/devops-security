ca2eae6d35bce128ee10834283ab7ce9fe0f220e
Benettonkkb <benettonkkb1@gmail.com>
2026-02-26 02:36:17 +0000

    Applying recommended role detail changes for plan and apply

--- terraform/aws-gha-oidc-providers.tf
+++ terraform/aws-gha-oidc-providers.tf
@@ -11,18 +11,39 @@ module "iam_oidc_gha_incubator" {
   ]
 
 }
-module "iam_oidc_incubator_tf_plan" {
-  source = "./modules/aws-gha-oidc-providers"
+resource "aws_iam_role" "incubator_tf_plan" {
+  name = "incubator-tf-plan"
 
-  role_name     = "incubator-tf-plan"
-  use_wildcard  = true
-  github_branch = "refs/heads/*" # concerning IAM audit, ok, as it is read-only
-  github_repo   = "hackforla/incubator"
-
-  policy_arns = [
-    "arn:aws:iam::aws:policy/ReadOnlyAccess"
-  ]
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Principal = {
+          Federated = module.iam_oidc_gha_incubator.provider_arn
+        }
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = [
+              "repo:hackforla/incubator:ref:refs/heads/*",
+              "repo:hackforla/incubator:pull_request"
+            ]
+          }
+        }
+      }
+    ]
+  })
 }
+
+resource "aws_iam_role_policy_attachment" "incubator_tf_plan_readonly" {
+  role      = aws_iam_role.incubator_tf_plan.name
+  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
+
 resource "aws_iam_role" "incubator_tf_apply" {
   name = "incubator-tf-apply"
 
@@ -40,7 +61,10 @@ resource "aws_iam_role" "incubator_tf_apply" {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
           }
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:hackforla/incubator:ref:refs/heads/main"
+            "token.actions.githubusercontent.com:sub" = [
+              "repo:hackforla/incubator:ref:refs/heads/main",
+              "repo:hackforla/incubator:pull_request"
+            ]
           }
         }
       }
@@ -50,6 +74,6 @@ resource "aws_iam_role" "incubator_tf_apply" {
 
 resource "aws_iam_role_policy_attachment" "incubator_tf_apply_admin" {
   role       = aws_iam_role.incubator_tf_apply.name
-  policy_arn  = "arn:aws:iam::aws:policy/AdministratorAccess"
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
