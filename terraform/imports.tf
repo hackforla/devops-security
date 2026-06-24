daf153f52f402a0ff7243ae3f93a31217acb2fcb
Ryan Sakuma <rsakuma4@gmail.com>
2026-06-24 16:00:12 -0700

    added import statements for policy attachments

--- terraform/imports.tf
+++ terraform/imports.tf
@@ -2,3 +2,12 @@ import {
 	to = aws_iam_group.ops_leads_group
 	id = "ops-leads"
 }
+
+import {
+ 	to = aws_iam_group_policy_attachment.admin
+	id = "ops-leads/arn:aws:iam::aws:policy/AdministratorAccess"
+}
+import {
+ 	to = aws_iam_group_policy_attachment.manageAccessKeys
+	id = "ops-leads/arn:aws:iam::035866691871:policy/ManageAccessKeys"
+}
